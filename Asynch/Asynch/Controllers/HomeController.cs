8f65e4b4c041ea7f775816562b85547e1ec6993c
dayan62 <dayanshaikh62@gmail.com>
2021-11-15 17:58:23 +0530

    asyncronysation

---
+++ Asynch/Asynch/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asynch.Controllers
+{
+    public class HomeController : Controller
+    {
+        SampleEntities1 db = new SampleEntities1();
+
+        // GET: Home
+        public  async Task<ActionResult> Index()
+        {
+            var List = await db.Employees.ToListAsync();
+
+            return View(List);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employees employees)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Employees.Add(employees);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+    }
+}
